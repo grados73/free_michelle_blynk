0ff60e68bf9cb1cb445ac85e84abd301df0608d1
grad73 <gradowski.kam@gmail.com>
2022-02-16 20:23:09 +0100

    Dodane miganie cykliczne fizyczna dioda.
    Dodana fizyczna dioda powodujaca cykliczne mruganie wraz ze zmianem stanu diody w blink.

--- Blynk__free_michelle_try1/src/main.cpp
+++ Blynk__free_michelle_try1/src/main.cpp
@@ -1,14 +1,15 @@
-#include <Arduino.h>
 /*************************************************************
 
-  This is a simple demo of sending and receiving some data.
-  Be sure to check out other examples!
+  Blynk using a LED widget on your phone!
+
+  App project setup:
+    LED widget on V1
  *************************************************************/
 
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
 #define BLYNK_TEMPLATE_ID           "TMPLS6OU4IQQ"
-#define BLYNK_DEVICE_NAME           "Quickstart Device"
+#define BLYNK_DEVICE_NAME           "GRAD Quickstart Device"
 #define BLYNK_AUTH_TOKEN            "6-UIFG5EkFyAbtLATzif3RagR20M8ey2"
 
 
@@ -26,33 +27,26 @@ char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "SuperExtraSzybkiInternetZuzy";
 char pass[] = "gradowscy6";
 
+WidgetLED led1(V1);
+
 BlynkTimer timer;
 
-// This function is called every time the Virtual Pin 0 state changes
-BLYNK_WRITE(V0)
-{
-  // Set incoming value from pin V0 to a variable
-  int value = param.asInt();
+// Real LED Definition
+#define LED 2
 
-  // Update state
-  Blynk.virtualWrite(V1, value);
-}
 
-// This function is called every time the device is connected to the Blynk.Cloud
-BLYNK_CONNECTED()
+// V1 LED Widget is blinking
+void blinkLedWidget()
 {
-  // Change Web Link Button message to "Congratulations!"
-  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
-}
-
-// This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
+  if (led1.getValue()) {
+    led1.off();
+    digitalWrite(LED, LOW);
+    Serial.println("LED on V1: off");
+  } else {
+    led1.on();
+    digitalWrite(LED, HIGH);
+    Serial.println("LED on V1: on");
+  }
 }
 
 void setup()
@@ -65,15 +59,15 @@ void setup()
   //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
   //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 
-  // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
+  pinMode(LED, OUTPUT);
+  
+  timer.setInterval(1000L, blinkLedWidget);
+
+  
 }
 
 void loop()
 {
   Blynk.run();
   timer.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
 }
